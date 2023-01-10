7810fc58bd52a52030441a8499f88fcb74a7fa76
Stephen Lingham <147378+StephenLingham@users.noreply.github.com>
2023-01-10 16:09:06 +0000

    Changed the grid rectangles to be squares. Also some minor code clean up

--- setup-grid.js
+++ setup-grid.js
@@ -1,8 +1,8 @@
 const grid = document.getElementById("grid");
 
-for (let i = 0; i < 100; i++) {
-    let square = document.createElement("div");
-    square.classList.add("square");
-    square.style.setProperty("--time", `${3 + Math.random() * 17}s`);
-    grid.appendChild(square);
+for (let i = 0; i < 400; i++) {
+  let square = document.createElement("div");
+  square.classList.add("square");
+  square.style.setProperty("--time", `${3 + Math.random() * 17}s`);
+  grid.appendChild(square);
 }
